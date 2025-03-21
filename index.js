7433592b078e198cafc1fc9b04d3838ce64eb25a
Sarah <sarah.wairimu@student.moringaschool.com>
2025-03-21 16:49:20 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression for divide
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Arrow function for square
+const square = (num) => num * num;
+
+// Arrow function for add
+const add = (a, b) => a + b;
